f799d32ecd011e8838dc78429322e5c51bfc5436
Achi Fernando <100669961+ALPHA-OFFICIAL-TEAM@users.noreply.github.com>
2022-04-13 19:22:05 +0530

    🎭🇱🇰 𝙰𝙻𝙿𝙷𝙰 🇱🇰🎭 v5

--- alphateam.js
+++ alphateam.js
@@ -3,3 +3,4 @@ module.exports = {
     TREX_TEAM: '94771039631,94784506970,94716184907,94711421243,94771251788', // Team - Trex TEAM
     TREX_OWNER: '94771039631' // Hirush Rukshan - Trex Owner --> For The Friendship And co Owner in Alpha 
 };
+
